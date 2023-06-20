ff627fe60acff08ae47034adbf8dbe6bb164bfce
byandrev <byandrev@gmail.com>
2023-06-19 21:00:53 -0500

    fix eslint problem

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -10,8 +10,8 @@ export default function ContextProvider({ children }) {
   const [isFocus, setIsFocus] = useState(true);
   const [counter, setCounter] = useState(null);
   const [time, setTime] = useState(null);
-  const [timeFocus, setTimeFocus] = useState(25);
-  const [timeBreak, setTimeBreak] = useState(5);
+  const [timeFocus] = useState(25);
+  const [timeBreak] = useState(5);
   const [timeDisplay, setTimeDisplay] = useState(`${timeFocus}:00`);
   const [intervalId, setIntervalId] = useState(0);
   const [running, setRunning] = useState(false);
